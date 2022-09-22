37f6f79c013cb440624760f8651043883842f964
V-arun03 <112625305+V-arun03@users.noreply.github.com>
2022-09-22 14:27:56 +0530

    Add files via upload

---
+++ 3.8.py
@@ -0,0 +1,7 @@
+test_list = ["eat","tea","tan","ate","hat","bat"]
+print("The original list : " + str(test_list))
+temp = defaultdict(list)
+for ele in test_list:
+    temp[str(sorted(ele))].append(ele)
+res = list(temp.values())
+print("The grouped Anagrams : " + str(res))
